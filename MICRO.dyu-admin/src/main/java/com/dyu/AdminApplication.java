8c5ad8528cae729439b0164cdbc987410bd6c09f
dyu <dyu@86links.com>
2018-08-07 16:35:05 +0800

    changed

--- MICRO.dyu-admin/src/main/java/com/dyu/AdminApplication.java
+++ MICRO.dyu-admin/src/main/java/com/dyu/AdminApplication.java
@@ -1,12 +1,25 @@
 package com.dyu;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping("/foo")
+    public String get() {
+        return foo;
     }
 }
