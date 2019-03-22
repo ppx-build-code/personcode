31e0768b37129cb4a98d0c929a6cfbd1cbb4db42
dyu <dyu@86links.com>
2019-03-23 01:37:37 +0800

    changed

--- SE.dyu-thread/src/com/dyu/lock/TestTwinsLock.java
+++ SE.dyu-thread/src/com/dyu/lock/TestTwinsLock.java
@@ -1,8 +1,44 @@
 package com.dyu.lock;
 
+import java.util.concurrent.locks.Lock;
+
 /**
  * @author dyu
  * @date 2018/12/02
  */
 public class TestTwinsLock {
+    public static void main(String[] args) throws InterruptedException {
+        final Lock lock = new TwinsLock();
+        class Worker extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                            Thread.sleep(1000);
+                            System.out.println(Thread.currentThread().getName());
+                            Thread.sleep(1000);
+
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Worker w = new Worker();
+            w.setDaemon(true);
+            w.start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(1000);
+            System.out.println();
+        }
+
+
+    }
 }
